ebd920267fc9f0f942febe3fcc4bf4d282cb0107
kawtar Elkhoumani <kawtar.elkhoumani@outlook.com>
2023-12-04 06:53:17 -0800

    list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for MyList class.'''
+
+
+class MyList(list):
+    '''Custom MyList Class.'''
+    def print_sorted(self):
+        '''Method for printing sorted list.'''
+        print(stored(self))
