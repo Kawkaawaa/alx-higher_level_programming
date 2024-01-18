aac68dda95974851ddfb0815a5fd3b2f52a57f23
kawtar Elkhoumani <kawtar.elkhoumani@outlook.com>
2024-01-18 13:45:47 -0800

    //

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create a database and a table according to specific descirption
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (states_id)
+	);
